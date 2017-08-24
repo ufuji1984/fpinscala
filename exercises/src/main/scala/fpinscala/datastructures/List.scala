fc72a14338b6fe60055c895d1d95cda2da143561
ufuji1984 <ufuji1984@gmail.com>
2017-08-24 18:48:50 +0900

    Exercise 3.2

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -49,8 +49,34 @@ object List { // `List` companion object. Contains functions for creating and wo
   def product2(ns: List[Double]) =
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
+  // Exercise 3.2
 
-  def tail[A](l: List[A]): List[A] = ???
+  /*
+  <Official answer>
+
+  Although we could return `Nil` when the input list is empty,
+  we choose to throw an exception instead.
+  This is a somewhat subjective choice.
+  In our experience, taking the tail of an empty list is often a bug,
+  and silently returning a value just means this bug will be discovered later,
+  further from the place where it was introduced.
+
+  It's generally good practice when pattern matching to use `_` for any
+  variables you don't intend to use on the right hand side of a pattern.
+  This makes it clear the value isn't relevant.
+  */
+  def tail[A](l: List[A]): List[A] =
+    l match {
+      case Nil => sys.error("tail of empty list")
+      case Cons(_,t) => t
+    }
+
+  def tail_myAnswer[A](l: List[A]): List[A] =
+    l match {
+      case Nil => Nil // or throw an exception?
+      case Cons(x, xs) => xs
+    }
+
 
   def setHead[A](l: List[A], h: A): List[A] = ???
 
