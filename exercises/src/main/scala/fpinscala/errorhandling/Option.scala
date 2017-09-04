0dfdec319caeebe0af526507f97be9877a6c8535
yu.fujisaki <yf@soinn.com>
2017-09-04 13:32:31 +0900

    Ex. 4.3

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -69,8 +69,7 @@ object Option {
     mean(xs) flatMap (m => mean(xs.map(x => math.pow(x - m, 2))))
 
 
-
-
+  // Ex. 4.3 TODO
   def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
 
   def sequence[A](a: List[Option[A]]): Option[List[A]] = ???
