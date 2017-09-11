aaae2a43917ab619369d0068b4e6d81bf51f8997
ufuji1984 <ufuji1984@gmail.com>
2017-09-11 18:30:29 +0900

    Ex. 4.6

--- exercises/src/main/scala/fpinscala/errorhandling/Either.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -1,16 +1,27 @@
 package fpinscala.errorhandling
 
-
-import scala.{Option => _, Either => _, Left => _, Right => _, _} // hide std library `Option` and `Either`, since we are writing our own in this chapter
+// hide std library `Option` and `Either`, since we are writing our own in this chapter
+import scala.{Option => _, Either => _, Left => _, Right => _, _}
 
 sealed trait Either[+E,+A] {
- def map[B](f: A => B): Either[E, B] = ???
 
- def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
+  // Ex. 4.6
+  def map[B](f: A => B): Either[E, B] = this match {
+    case Left(e) => Left(e) // これは再度作ることにはならないのか？宣言がobjectだからSingleton的な動き？
+    case Right(a) => Right(f(a))
+  }
 
- def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = ???
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Left(e) => Left(e)
+    case Right(a) => f(a)
+  }
 
- def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case Left(_) => b
+    case Right(a) => Right(a)
+  }
+
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
 }
 case class Left[+E](get: E) extends Either[E,Nothing]
 case class Right[+A](get: A) extends Either[Nothing,A]
@@ -20,13 +31,13 @@ object Either {
 
   def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = ???
 
-  def mean(xs: IndexedSeq[Double]): Either[String, Double] = 
-    if (xs.isEmpty) 
+  def mean(xs: IndexedSeq[Double]): Either[String, Double] =
+    if (xs.isEmpty)
       Left("mean of empty list!")
-    else 
+    else
       Right(xs.sum / xs.length)
 
-  def safeDiv(x: Int, y: Int): Either[Exception, Int] = 
+  def safeDiv(x: Int, y: Int): Either[Exception, Int] =
     try Right(x / y)
     catch { case e: Exception => Left(e) }
 
