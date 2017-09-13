61d87e90875b07415a795d41886f9c08a9002e1f
ufuji1984 <ufuji1984@gmail.com>
2017-09-13 18:45:20 +0900

    Ex. 5.2 途中

--- exercises/src/main/scala/fpinscala/05_laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/05_laziness/Stream.scala
@@ -3,6 +3,21 @@ package fpinscala.laziness
 import Stream._
 trait Stream[+A] {
 
+  def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
+    this match {
+      case Cons(h,t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
+      case _ => z
+    }
+
+  def exists(p: A => Boolean): Boolean =
+    foldRight(false)((a, b) => p(a) || b) // Here `b` is the unevaluated recursive step that folds the tail of the stream. If `p(a)` returns `true`, `b` will never be evaluated and the computation terminates early.
+
+  @annotation.tailrec
+  final def find(f: A => Boolean): Option[A] = this match {
+    case Empty => None
+    case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
+  }
+
   // Ex. 5.1
   def toList_recursive: List[A] = this match {
     //mine (compile error: value toList is not a member of () => fpinscala.laziness.Stream[A])
@@ -21,24 +36,19 @@ trait Stream[+A] {
     go(this, List()).reverse
   }
 
-
-  def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
-    this match {
-      case Cons(h,t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
-      case _ => z
-    }
-
-  def exists(p: A => Boolean): Boolean =
-    foldRight(false)((a, b) => p(a) || b) // Here `b` is the unevaluated recursive step that folds the tail of the stream. If `p(a)` returns `true`, `b` will never be evaluated and the computation terminates early.
-
-  @annotation.tailrec
-  final def find(f: A => Boolean): Option[A] = this match {
-    case Empty => None
-    case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
-  }
   def take(n: Int): Stream[A] = ???
 
-  def drop(n: Int): Stream[A] = ???
+  def drop(n: Int): Stream[A] = {
+
+    def go(nn: Int, acc: => Stream[A]): Stream[A] = {
+      if (nn <= 0) acc
+      else acc match {
+        case Cons(h, t) => go(nn-1, t)
+        
+      }
+    }
+  }
+
 
   def takeWhile(p: A => Boolean): Stream[A] = ???
 
