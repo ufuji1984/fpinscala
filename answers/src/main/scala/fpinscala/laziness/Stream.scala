10264a61334f111854e18f9d08e107fd20f13b21
yu.fujisaki <yf@soinn.com>
2017-09-15 10:32:44 +0900

    Ex. 5.14

--- answers/src/main/scala/fpinscala/laziness/Stream.scala
+++ answers/src/main/scala/fpinscala/laziness/Stream.scala
@@ -160,8 +160,11 @@ trait Stream[+A] {
       case (Cons(h1, t1), Cons(h2, t2)) => Some(f(Some(h1()), Some(h2())) -> (t1() -> t2()))
     }
 
-  /*
-  `s startsWith s2` when corresponding elements of `s` and `s2` are all equal, until the point that `s2` is exhausted. If `s` is exhausted first, or we find an element that doesn't match, we terminate early. Using non-strictness, we can compose these three separate logical steps--the zipping, the termination when the second stream is exhausted, and the termination if a nonmatching element is found or the first stream is exhausted.
+  /* Ex. 5.14
+  `s startsWith s2` when corresponding elements of `s` and `s2` are all equal, until the point that `s2` is exhausted.
+  If `s` is exhausted first, or we find an element that doesn't match, we terminate early.
+  Using non-strictness, we can compose these three separate logical steps--the zipping, the termination when the second
+  stream is exhausted, and the termination if a nonmatching element is found or the first stream is exhausted.
   */
   def startsWith[A](s: Stream[A]): Boolean =
     zipAll(s).takeWhile(!_._2.isEmpty) forAll {
