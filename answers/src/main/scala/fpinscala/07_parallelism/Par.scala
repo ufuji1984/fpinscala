33d2c18e816176e984b3bb0510a23d8694230f76
yu.fujisaki <yf@soinn.com>
2017-09-21 17:11:40 +0900

    Ex. 7.1

--- answers/src/main/scala/fpinscala/07_parallelism/Par.scala
+++ answers/src/main/scala/fpinscala/07_parallelism/Par.scala
@@ -18,11 +18,24 @@ object Par {
     def cancel(evenIfRunning: Boolean): Boolean = false
   }
 
-  def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] = // `map2` doesn't evaluate the call to `f` in a separate logical thread, in accord with our design choice of having `fork` be the sole function in the API for controlling parallelism. We can always do `fork(map2(a,b)(f))` if we want the evaluation of `f` to occur in a separate thread.
+  // Ex. 7.1
+  /*
+   `map2` doesn't evaluate the call to `f` in a separate logical thread, in accord with
+   our design choice of having `fork` be the sole function in the API for controlling
+   parallelism. We can always do `fork(map2(a,b)(f))` if we want the evaluation of `f`
+   to occur in a separate thread.
+    */
+  def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] =
     (es: ExecutorService) => {
       val af = a(es)
       val bf = b(es)
-      UnitFuture(f(af.get, bf.get)) // This implementation of `map2` does _not_ respect timeouts. It simply passes the `ExecutorService` on to both `Par` values, waits for the results of the Futures `af` and `bf`, applies `f` to them, and wraps them in a `UnitFuture`. In order to respect timeouts, we'd need a new `Future` implementation that records the amount of time spent evaluating `af`, then subtracts that time from the available time allocated for evaluating `bf`.
+      UnitFuture(f(af.get, bf.get))
+      // This implementation of `map2` does _not_ respect timeouts.
+      // It simply passes the `ExecutorService` on to both `Par` values, waits for the
+      // results of the Futures `af` and `bf`, applies `f` to them, and wraps them in
+      // a `UnitFuture`. In order to respect timeouts, we'd need a new `Future`
+      // implementation that records the amount of time spent evaluating `af`, then
+      // subtracts that time from the available time allocated for evaluating `bf`.
     }
 
   def fork[A](a: => Par[A]): Par[A] = // This is the simplest and most natural implementation of `fork`, but there are some problems with it--for one, the outer `Callable` will block waiting for the "inner" task to complete. Since this blocking occupies a thread in our thread pool, or whatever resource backs the `ExecutorService`, this implies that we're losing out on some potential parallelism. Essentially, we're using two threads when one should suffice. This is a symptom of a more serious problem with the implementation, and we will discuss this later in the chapter.
